8d98c053cfbd0018e07abd088037599ae7ca3a36
silvasigmaboy <chrischarp013@gmail.com>
2026-04-16 23:17:12 +0000

    remove axios

--- tz_px_noproxy.js
+++ tz_px_noproxy.js
@@ -1,24 +1,19 @@
-import axios from "axios";
-import "dotenv/config";
-
 export const checkTz = async (username) => {
-  // Properly formatted proxy URL
-
   try {
-    const response = await axios.get(
+    const response = await fetch(
       "https://worker-purple-wind-1de7.idrissimahdi2020.workers.dev",
       {
-        timeout: 10000,
+        signal: AbortSignal.timeout(10000),
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         },
       },
     );
-    const ipDetails = { timezone: response.data.trim() };
-    return ipDetails?.timezone || undefined;
+    const data = await response.text();
+    const timezone = data.trim();
+    return timezone || undefined;
   } catch (error) {
-    // console.error("Error fetching timezone:", error.message);
     return undefined;
   }
 };
